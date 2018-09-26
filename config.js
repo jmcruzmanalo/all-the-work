ab928153013b8daf05d0d29483cf21f0490698b8
Juan Miguel Manalo <41225365+jmcruzmanalo@users.noreply.github.com>
2018-09-26 22:48:56 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  DEV_MODE: false,
-  BotToken: 'NDY1ODU3MDcxMDk1NzQyNDk1.DiTmuQ.kriHkhbB7uXxd8t_Prg_9oTZuv0',
-  serverId: '415506140840067076',
-  developerDiscordId: '359314226214600704'
-};
